0261385b89eeeb0f66f0d16a04192e1b9ff4f49a
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-04-10 18:17:44 +0530

    added two view files

---
+++ resources/views/formpro.blade.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+<div class="row container">
+        <h2 class="header center">SIGN UP</h2>
+</div>
+<div class="row">
+    <form class="col s8 offset-s2">
+      <div class="row">
+        <div class="input-field col s6">
+          <i class="material-icons prefix">account_circle</i>
+          <input id="icon_prefix" type="text" class="validate">
+          <label for="icon_prefix">First Name</label>
+        </div>
+        <div class="input-field col s6">
+          <i class="material-icons prefix">phone</i>
+          <input id="icon_telephone" type="tel" class="validate">
+          <label for="icon_telephone">Telephone</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s6">
+          <i class="material-icons prefix">email</i>
+          <input id="icon_prefix" type="email" class="validate">
+          <label for="icon_prefix">E-mail</label>
+        </div>
+        <div class="input-field col s6">
+          <i class="material-icons prefix">home</i>
+          <input id="icon_telephone" type="text" class="validate">
+          <label for="icon_telephone">Address</label>
+        </div>
+      </div>
+      <div class="row">
+      <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+    <i class="material-icons right">send</i>
+  </button>
+      </div>
+    </form>
+  </div>
+  </body>
+  </html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,97 @@
+    <html>
+    <head>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  
+    </head>
+    <body>
+    <ul id="dropdown1" class="dropdown-content">
+    <li><a href="#!">Monthly Plan</a></li>
+    <li><a href="#!">Yearly Plan</a></li>
+    </ul>
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Fitness Era</a>
+      <ul  class="right hide-on-med-and-down">
+        <li><a class="dropdown-trigger" href="formpro.html" data-target="dropdown1">Sign Up<i class="material-icons right">arrow_drop_down</i></a></li>
+        <li><a href="sass.html">Log In</a></li>
+        <li><a href="badges.html">About Us</a></li>
+        <li><a href="collapsible.html">Contact Us</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="parallax-container">
+      <div class="parallax"><img src="img/fitness.jpg"></div>
+    </div>
+    <div class="section white">
+      <div class="row container">
+        <h2 class="header">Fitness Era welcomes you !!</h2>
+        <p class="grey-text text-darken-3 lighten-3">We are happy to serve you.</div>
+    </div>
+    <div class="parallax-container">
+      <div class="parallax"><img src="images/fitness2.jpg"></div>
+    </div>
+    </body>
+    <div class="row">
+    <div class="col s6 m4 offset-m1">
+      <div class="card">
+        <div class="card-image">
+          <img src="img/fitness3.jpg">
+          <span class="card-title">Monthly plan</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+        </div>
+        <div class="card-content">
+          <p>Explore the offers and benefits of monthly membership.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s6 m4 offset-m1">
+      <div class="card">
+        <div class="card-image">
+          <img src="img/fitness4.jpg">
+          <span class="card-title">Yearly plan</span>
+          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+        </div>
+        <div class="card-content">
+          <p>Be a part of our family and enjoy exclusive offers.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+    <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Mail Us :</h5>
+                <p class="grey-text text-lighten-4">Fitness Era Limited,
+
+Buildings Alyssa, Begonia &
+
+Clove Embassy Tech Village,
+
+Outer Ring Road, Devarabeesanahalli Village,
+
+Noida, 560103,
+ India.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Info</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-5" href="#!">Bio</a></li>
+                  <li><a class="grey-text text-lighten-5" href="#!">Address</a></li>
+                  <li><a class="grey-text text-lighten-5" href="#!">Contact</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © www.fitnessera.com
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
+    </html>
